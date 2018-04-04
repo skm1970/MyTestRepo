f7c1ed8038ebdd48b3aa4f044ea463a007fb8073
Sunil K M <sunil.morakkatte@gmail.com>
2018-04-04 00:25:06 -0400

    Files Added

---
+++ ts-hello/main.js
@@ -0,0 +1,12 @@
+function log(message) {
+    console.log(message);
+}
+var message = "Hellow World";
+log(message);
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log("Finally: " + i);
+}
+doSomething();
